015e6dd45690d996d50d65cec75d39558ad3859b
Megan Chamberlin <96836821+Piichii-Meg@users.noreply.github.com>
2022-04-11 15:55:37 -0400

    Add files via upload

--- mail.php
+++ mail.php
@@ -7,5 +7,5 @@ $recipient = "megansachamberlin@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
